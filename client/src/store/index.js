abf15569cb65d08e35af049d47cf0cb52bbabbbb
scarecrow-11 <hafizurmilon.11@gmail.com>
2020-05-08 01:22:58 +0600

    Pre-deploy with Only Translation.

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,8 +5,8 @@ import thunk from 'redux-thunk'
 const middleware = [thunk]
 
 const store = createStore(rootReducer, compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
+    //,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
 export default store
