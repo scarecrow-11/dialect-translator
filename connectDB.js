b57064731075625085b9d240b8816e6d69192aa2
scarecrow-11 <hafizurmilon.11@gmail.com>
2020-05-08 15:30:31 +0600

    Pre-deployment with ENV vars + Translation Only.

--- connectDB.js
+++ connectDB.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = () => {
     const localURI = 'mongodb://localhost/dialect-translator'
-    const cloudURI = 'mongodb+srv://'+process.env.dbUsername+':'+process.env.dbPassword+'@dictionary-74kzq.mongodb.net/dialect-translator?retryWrites=true&w=majority'
-    mongoose.connect(cloudURI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    mongoose.connect(process.env.MONGODB_URI || localURI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
         console.log('Database connected...')
     })
 }
